0b61451017c2e533f9e3e047759bedeac6755193
aditiverma1009 <aditi_verma@mckinsey.com>
2019-12-07 16:31:14 +0530

    boilerplate

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        hi
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+       basic
+      </div>
+    );
+  }
+}
+
+export default App;
